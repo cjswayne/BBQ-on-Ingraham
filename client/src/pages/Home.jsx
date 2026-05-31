ac539f5869e3e7bf9c3272a2380862943717e640
Charles Swayne <johnnycake215@gmail.com>
2026-05-31 16:59:15 -0700

    update the text on the homepage

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -146,7 +146,7 @@ const HeroContent = ({ eventDate }) => {
             <path d="M19 3v12h-5c-.023 -3.681 .184 -7.406 5 -12zm0 12v6h-1v-3m-10 -14v17m-3 -17v3a3 3 0 1 0 6 0v-3" />
           </svg>
           <div>
-            <p className="font-medium leading-none">Bring a dish</p>
+            <p className="font-medium leading-none">Bring a dish & a chair!</p>
             <p className="mt-1 text-xs opacity-70">Themed each week — vote below</p>
           </div>
         </div>
